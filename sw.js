7e868348cf7e5d8e766333fbce3450ac39afac34
melkhafef <42751023+melkhafef@users.noreply.github.com>
2018-12-01 23:12:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open('resturant-cache').then(function(cache){
+            cache.addAll([
+                '/',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                '/index.html',
+                '/restaurant.html',
+                '/css/responsive.css',
+                '/css/styles.css',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg'
+            ])
+        })
+    )
+})
+self.addEventListener('fetch',function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response) return response;
+            return fetch(event.request);
+        })
+    )
+})
